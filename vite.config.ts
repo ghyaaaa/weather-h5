5ffa63796790ea5f69b619c4bee3c0b52c1a37fa
龚航宇 <785567192@qq.com>
2023-07-01 00:40:14 +0800

    feat: 接入postcss2rem

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,13 @@
 import { defineConfig } from "vite";
+import px2rem from "postcss-plugin-px2rem";
 import react from "@vitejs/plugin-react";
+
+const px2remOption = {
+  rootValue: 14.4,
+  unitPrecision: 5,
+  mediaQuery: false,
+  minPixelValue: 0,
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,6 +23,9 @@ export default defineConfig({
       scopeBehaviour: "local",
       generateScopedName: "[name]_[local]_[hash:5]",
       globalModulePaths: [],
+    },
+    postcss: {
+      plugins: [px2rem(px2remOption)],
     },
   },
   server: {
