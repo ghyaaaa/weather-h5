256239a7f5ed13234c208d26fcbabf3e6f7264d9
龚航宇 <785567192@qq.com>
2023-07-01 12:51:13 +0800

    fix: 修改casts 内容

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
-import { Header, WeatherBody } from "./component";
+import { Header, WeatherBody, WeatherDay } from "./component";
 import Style from "./App.module.less";
+import { useState, useMemo } from "react";
+import * as dayjs from "dayjs";
+import * as isToday from "dayjs/plugin/isToday";
+import { Forecasts, Weather, Casts } from "./type";
+
+dayjs.extend(isToday);
 
 function App() {
+  const [forecastData, setForecastData] = useState<Forecasts[]>();
+
+  const getWeatherData = (data: Weather) => {
+    console.log(data);
+    setForecastData(data.forecasts);
+  };
+
+  // 获取当天天气情况
+  const todayWeatherData = useMemo(() => {
+    const firstForecast = forecastData?.[0];
+    const reporttime = firstForecast?.reporttime;
+    const castsItem: any = (forecastData?.[0].casts || []).find((item) =>
+      dayjs(item.date).isToday()
+    );
+    return {
+      ...castsItem,
+      reporttime,
+    };
+  }, [forecastData]);
+
   return (
     <div className={Style.layout}>
-      <Header />
-      <WeatherBody />
+      <Header getWeatherData={getWeatherData} />
+      <WeatherBody data={todayWeatherData} />
+      <WeatherDay data={forecastData} />
     </div>
   );
 }
